6f3a288e659f36a0663bc49e98dbf921b62abf5f
kaane5662 <kaane0169@gmail.com>
2024-02-29 19:24:36 -0500

    landing page bug and cookies hopefully fixed

--- client/ticketing-platform/src/components/Landing/FAQ.jsx
+++ client/ticketing-platform/src/components/Landing/FAQ.jsx
@@ -4,11 +4,11 @@ import Question from "./Question";
 
 export default function FAQ(){
     return(
-        <main className=" min-h-screen bg-primary text-secondary font-poppins flex justify-center items-center">
-            <div className = "p-32 gap-12 flex items-center justify-center ">
+        <main className=" min-h-screen bg-primary text-secondary font-poppins flex justify-center items-center w-[100%]">
+            <div className = "p-32 max-lg:px-0 gap-12 flex items-center justify-center ">
                 <div className="flex flex-col gap-8">
                     <h1 className="text-5xl font-bold text-center max-lg:text-4xl">You have a <span className="text-complementary">Question?</span> </h1>
-                    <div className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-4 ">
                         <Question question={"How do I join as a seller?"} answer={"Just sign up with an account on our platform and then follow the boarding instructions when you click Switch to Seller."}></Question>
                         <Question question={"What do I need to join as a seller?"} answer={"A valid SSN is required for identification purposes, and to facilitate transactions, you will need either a banking account or a debit/credit card. However, please note that we do not store your SSN within our system for security reasons."}></Question>
                         <Question question={"What are the benefits of using SwftTs?"} answer={"We help you focus on hosting the best event while leaving the payment stress to us."}></Question>
